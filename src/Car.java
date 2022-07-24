126c9875299dec46c91f1f2fde8361e03f50339f
Kinga Bielicka <kinga.orzeszke@gmail.com>
2022-07-24 11:19:31 +0200

    kalkulator; dodawanie, odejmowanie, mnożenie

--- src/Car.java
+++ src/Car.java
@@ -3,48 +3,50 @@ public class Car {
     private int doorsAmount;
     private String brand;
     private boolean automaticGearbox;
-    // boolean odpowiada za true or false
+
 
     public Car(String colour, int doorsAmount, String brand, boolean automaticGearbox) {
         this.colour = colour;
         this.doorsAmount = doorsAmount;
         this.brand = brand;
         this.automaticGearbox = automaticGearbox;
-// konstruktor 4-parametrowy powyżej; nic nie zwraca, nic nie przyjmuje; to w nawiasie to parametry, a ciało metody znajduje się w wąsach
     }
 
-    public void setAutomaticGearbox(boolean automaticGearbox) {
-        this.automaticGearbox = automaticGearbox;
-    }
-
-    public boolean getAutomaticGearbox() {
-        return automaticGearbox;
-    }
-
-
-    public void setColour(String colour) {
+    public Car(String colour, int doorsAmount, String brand) {
         this.colour = colour;
+        this.doorsAmount = doorsAmount;
+        this.brand = brand;
     }
 
     public String getColour() {
         return colour;
     }
 
-    public void setDoorsAmount(int doorsAmount) {
-        this.doorsAmount = doorsAmount;
+    public void setColour(String colour) {
+        this.colour = colour;
     }
 
     public int getDoorsAmount() {
         return doorsAmount;
     }
 
-
-    public void setBrand(String brand) {
-        this.brand = brand;
+    public void setDoorsAmount(int doorsAmount) {
+        this.doorsAmount = doorsAmount;
     }
 
     public String getBrand() {
         return brand;
     }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public boolean getAutomaticGearbox() {
+        return automaticGearbox;
+    }
+
+    public void setAutomaticGearbox(boolean automaticGearbox) {
+        this.automaticGearbox = automaticGearbox;
+    }
 }
